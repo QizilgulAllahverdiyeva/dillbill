92d00797d01cb436362b6ba46e4ad39bf48516b6
QizilgulAllahverdiyeva <162086153+QizilgulAllahverdiyeva@users.noreply.github.com>
2024-04-01 06:46:49 -0800

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,63 @@
+//Task-1
+let firstNumber = 67;
+let x = firstNumber / 10;
+let y = parseInt (x);
+let z = firstNumber % 10;
+console.log("Task-1 Output:", y , "" , z);
+
+
+
+//Task-2
+let selectNumber = 256;
+let p = selectNumber / 100;
+let integer = parseInt (p);
+let s = selectNumber % 25;
+let result = integer * 10 + s
+console.log("Task-2 Output:" , result);
+
+//Task-3
+let n = 28;
+let k = 270;
+let qalanAlma = k % n;
+console.log("Task-3 Output (qalanAlma):", qalanAlma);
+
+//Task-5 
+let a = 8;
+let b = 280;
+let perimetr = 2 * (a+b)
+console.log("Task-5 Output Perimetr:", perimetr);
+
+//Task-7
+let m = 865;
+let l = 10 ;
+let hasil = m*l;
+console.log("Task-7 Output (hasil):", hasil);
+
+
+//Task-8 
+let t = 25;
+let r = 2 ;
+let quvvet = t**r;
+console.log("Task-8 Output (quvvet):", quvvet);
+
+//Task-9
+let inputNum = 254 ;
+let lastNum = inputNum % 25;
+let result2 = (inputNum - lastNum)/10
+console.log("Task-9 Output:", result2);
+
+//Task-10 
+let inputNatural = 89;
+let outputNatural = inputNatural % 10;
+console.log("Task-10 Output :", outputNatural );
+
+//Task-11
+let str = [5,8,9,7];
+let strReverse = str.reverse()
+console.log("Task-11 Output :", str.join(''));
+
+//Task-12 
+let pul = 90;
+let pirojki = 25;
+let qalanPul = pul % pirojki;
+console.log("Task-12 Output (Hesenin qalan pulu):", qalanPul);
